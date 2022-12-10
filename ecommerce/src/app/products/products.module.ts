95807924ec2f4d0a30edf2481e00d88eb1a0e11f
Yasein H. Burqan <yaseinhaniburqan@gmail.com>
2022-12-10 23:13:12 +0000

    creat-header-and-categories

---
+++ ecommerce/src/app/products/products.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AllProductsComponent } from './components/all-products/all-products.component';
+import { ProductsDetailsComponent } from './components/products-details/products-details.component';
+import { SharedModule } from '../shared/shared.module';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+  declarations: [AllProductsComponent, ProductsDetailsComponent],
+  imports: [BrowserModule, CommonModule, SharedModule],
+})
+export class ProductsModule {}
